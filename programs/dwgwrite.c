eed44af6f70dfe1886203b23e9fc13deaa39fa0c
Reini Urban <rurban@cpan.org>
2020-04-26 12:58:59 +0200

    set dat.from_version in converters
    esp. with in_json.
    this enables IF_ENCODE_FROM_EARLIER, e.g. in r2004_file_header.spec

--- programs/dwgwrite.c
+++ programs/dwgwrite.c
@@ -1,7 +1,7 @@
 /*****************************************************************************/
 /*  LibreDWG - free implementation of the DWG file format                    */
 /*                                                                           */
-/*  Copyright (C) 2018-2019 Free Software Foundation, Inc.                   */
+/*  Copyright (C) 2018-2020 Free Software Foundation, Inc.                   */
 /*                                                                           */
 /*  This library is free software, licensed under the terms of the GNU       */
 /*  General Public License as published by the Free Software Foundation,     */
@@ -65,9 +65,9 @@ help (void)
   printf ("  -v[0-9], --verbose [0-9]  verbosity\n");
   printf ("  --as rNNNN                save as version\n");
   printf ("           Valid versions:\n");
-  printf ("             r12, r14, r2000 (default)\n");
+  printf ("             r12, r14, r2000 (default), r2004, r2010, r2013, r2018\n");
   printf ("           Planned versions:\n");
-  printf ("             r9, r10, r11, r2004, r2007, r2010, r2013, r2018\n");
+  printf ("             r9, r10, r11, r2007\n");
   printf ("  -I fmt,  --format fmt     DXF, DXFB, JSON\n");
   printf (
       "           Planned input formats: GeoJSON, YAML, XML/OGR, GPX\n");
@@ -79,10 +79,10 @@ help (void)
 #else
   printf ("  -v[0-9]     verbosity\n");
   printf ("  -a rNNNN    save as version\n");
-  printf ("              Valid versions:\n");
-  printf ("                r12, r14, r2000 (default)\n");
-  printf ("              Planned versions:\n");
-  printf ("                r9, r10, r11, r2004, r2007, r2010, r2013, r2018\n");
+  printf ("           Valid versions:\n");
+  printf ("             r12, r14, r2000 (default), r2004, r2010, r2013, r2018\n");
+  printf ("           Planned versions:\n");
+  printf ("             r9, r10, r11, r2007\n");
   printf ("  -I fmt      fmt: DXF, DXFB\n");
   printf ("              Planned input formats: GeoJSON, YAML, XML/OGR, GPX\n");
   printf ("  -o dwgfile\n");
